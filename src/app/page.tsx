9e28b87257028ebb36e7c40d3d29204304acff0a
kyle-pazdel <kyle.kayvaun@gmail.com>
2024-04-13 09:52:27 -0700

    Adds text and image to main page, adds favicon, adds controlled theme background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import { Center, Text, Image } from "@mantine/core";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div></div>
-    </main>
+    <Center>
+      <Text c="white">geneva karr is trash.</Text>
+      <Image
+        alt="polaroid of geneva karr in 3/4 scope with her signature at the bottom"
+        src="/geneva-karr.jpg"
+        width="64"
+        height="64"
+      />
+    </Center>
   );
 }
