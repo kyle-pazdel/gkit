1358bb89ca78abd0c3cad6be1665630d091dc754
kyle-pazdel <kyle.kayvaun@gmail.com>
2024-04-13 10:12:53 -0700

    Adjusts position of text and image, and resizes image on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { anton } from "./fonts";
 
 export default function Home() {
   return (
-    <Center m="xl" p="xl" maw={300}>
+    <Center m="xl" p="xl">
       <Flex justify="center" align="center" direction="column" gap="xl">
         <Text className={anton.className} c="white">
           geneva karr is trash.
@@ -11,8 +11,8 @@ export default function Home() {
         <Image
           alt="polaroid of geneva karr in 3/4 scope with her signature at the bottom"
           src="/geneva-karr.jpg"
-          width={40}
-          height={40}
+          w={150}
+          h={150}
         />
       </Flex>
     </Center>
